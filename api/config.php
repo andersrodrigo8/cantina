d37ebadbe158877c82b436383fb5d42d0426b7c7
andersrodrigo8 <andersrodrigo8@gmail.com>
2024-01-26 13:25:16 -0300

    copinha2

--- api/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define('DB_HOST', 'localhost');
-    define('DB_NAME','copinha');
-    define('DB_PORT','5432');
-    define('DB_SGBD','pgsql');
-    define('DB_USER','postgres');
-    define('DB_PASS','1273914');
--- api/rodape.php
+++
@@ -1,9 +0,0 @@
-<!-- PAGE FOOTER -->
-<div class="page-footer">
-	<div class="row">
-		<div class="col-xs-12 col-sm-6">
-			<span class="txt-color-white">Copinha 1.0 <span class="hidden-xs"> - Web Xupisco</span> © 2023-<?php echo date('Y'); ?></span>
-		</div> 
-	</div>
-</div>
-<!-- END PAGE FOOTER -->
